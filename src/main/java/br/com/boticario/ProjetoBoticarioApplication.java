b7603359a420094a711c01b2984fd0248fd9245c
JessiiPer <jessica_roch@hotmail.com>
2019-12-01 16:09:54 -0300

    Projeto criado

---
+++ src/main/java/br/com/boticario/ProjetoBoticarioApplication.java
@@ -0,0 +1,13 @@
+package br.com.boticario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoBoticarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoBoticarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/boticario/ProjetoBoticarioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.boticario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoBoticarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
